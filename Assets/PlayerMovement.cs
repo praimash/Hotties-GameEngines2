76b9dfccf5a0d50089ab61e5a3477af77b202ad9
Zeynep Belinay Koçaker <zbkocaker@gmail.com>
2025-12-12 19:30:39 +0300

    ev planı tamam
    birlikte karakter yükleyip evde gezicez, game feele bakıp cilası için devam edicem

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,52 +1,37 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // 1. BU KÜTÜPHANEYÝ EKLEMEK ZORUNDASIN
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    // 1. Dosya ismin 'NewActions' olduðu için deðiþken türü bu olmalý:
+    private NewActions inputActions;
 
-    // Oluþturduðumuz o otomatik class'tan bir tane tanýmlýyoruz.
-    // (Senin dosyanýn adý neyse 'GameInputs' yerine onu yaz)
-    private PlayerMovement inputActions;
-
-    // OYUN BAÞLAMADAN ÖNCE (Awake)
     private void Awake()
     {
-        // Tuþ ayarlarýný yükle (New'le)
-        inputActions = new PlayerMovement();
+        // 2. Sýnýfý oluþtururken de dosya ismini kullanýyoruz:
+        inputActions = new NewActions();
     }
 
-    // OBJE AKTÝF OLUNCA (OnEnable) - ÇOK ÖNEMLÝ
     private void OnEnable()
     {
-        // Girdi sistemini "Dinlemeye Baþla"
         inputActions.Enable();
     }
 
-    // OBJE KAPANINCA (OnDisable) - ÇOK ÖNEMLÝ
     private void OnDisable()
     {
-        // Girdi sistemini "Dinlemeyi Býrak" (Hata almamak için þart)
         inputActions.Disable();
     }
 
     void Update()
     {
-        // --- ESKÝ YÖNTEM (SÝLÝNDÝ) ---
-        // float x = Input.GetAxis("Horizontal");
-        // ...
+        // 3. ÝÞTE BURASI ÇOK ÖNEMLÝ!
+        // Eriþim Yolu: [Deðiþken].[HaritaÝsmi].[AksiyonÝsmi]
+        // Senin haritanýn adý da "Move", aksiyonun adý da "Move".
+        // Bu yüzden "Move.Move" yazacaðýz.
 
-        // --- YENÝ YÖNTEM ---
-        // 1. Veriyi Oku:
-        // "Player" haritasýndaki "Move" aksiyonunu Vector2 olarak oku.
-        // Bize (x, y) deðerleri verecek (Örn: W'ye basýnca y=1 olur).
-        Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
+        Vector2 inputVector = inputActions.Move.Move.ReadValue<Vector2>();
 
-        // 2. 3D Dünyaya Çevir:
-        // 2D'deki Y (Yukarý), 3D dünyada Z (Ýleri) demektir.
+        // (Geri kalan kodlar ayný...)
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-
-        // 3. Hareketi Uygula:
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
